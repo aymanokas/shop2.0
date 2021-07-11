b8c3d98f03cb0ac92277a13a3bb7df6faddcc3fd
Aymen <aymen@alphorm.com>
2021-07-11 18:04:14 +0100

    add get shoping cart

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,8 +1,21 @@
 import express from 'express'
+import fetch from 'node-fetch'
+import { BASE_URL, X_API_KEY } from '../settings'
 const cart = express.Router()
 
-cart.get('/', (req, res) => {
-  res.send('cart itmes')
+cart.get('/getShopingCart/:userId', async (req, res) => {
+  const userId = req.params.userId
+  const query = `${BASE_URL}/shopingcart?q={"userId": "${userId}"}`
+  const result = await fetch(query, {
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-apikey': X_API_KEY
+    }
+  })
+  const json = await result.json()
+  if (json.length) res.send(json[0].products)
+  else res.send({ error: true, type: 'no shoping cart exist' })
 })
 
 export default cart
