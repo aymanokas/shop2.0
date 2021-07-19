19b8eb2a99fcfe1a1dd7a92f40fdaffbcaf1220c
amine <amine@alphorm.com>
2021-07-19 01:43:28 +0100

    store example

--- src/pages/Catalog/store.js
+++ src/pages/Catalog/store.js
@@ -1,10 +1,34 @@
-const initialState = {
+import { takeLatest, put } from 'redux-saga/effects'
+const initialUserState = {
 
 }
 
-export const reducer = (state = initialState, { type, data }) => {
+export const reducer = (state = initialUserState, { type, data }) => {
   switch (type) {
+    case GET_CATALOG_SUCCESS:
+      return {
+        ...state,
+        catalog: data
+      }
     default:
       return state
   }
 }
+
+export const getCatalogAction = () => ({ type: GET_CATALOG_SUCCESS })
+
+function * fetchCatalog () {
+  try {
+    yield put({ type: GET_CATALOG_SUCCESS, data: 'hello' })
+  } catch (err) {
+    yield put({ type: GET_CATALOG_FAILED, data: err })
+  }
+}
+
+export function * trainerRootSaga () {
+  yield takeLatest(GET_CATALOG_REQUESTED, fetchCatalog)
+}
+
+const GET_CATALOG_SUCCESS = 'GET_CATALOG_SUCCESS'
+const GET_CATALOG_FAILED = 'GET_CATALOG_FAILED'
+const GET_CATALOG_REQUESTED = 'GET_CATALOG_REQUESTED'
